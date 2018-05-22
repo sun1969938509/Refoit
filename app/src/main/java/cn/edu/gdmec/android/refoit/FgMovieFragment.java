cf08c8a5b97044ab51ffc34058d3e88623bd708d
sun1969938509 <1969938509@qq.com>
2018-05-22 17:25:24 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/refoit/FgMovieFragment.java
@@ -0,0 +1,26 @@
+package cn.edu.gdmec.android.refoit;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by apple on 18/5/22.
+ */
+
+public class FgMovieFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fg_movie,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
