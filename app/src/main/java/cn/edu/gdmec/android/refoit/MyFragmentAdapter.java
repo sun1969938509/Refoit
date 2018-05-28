d495a17b54b22081c2a597afe9955249041e8aba
sun1969938509 <1969938509@qq.com>
2018-05-28 22:10:40 +0800

    的

--- app/src/main/java/cn/edu/gdmec/android/refoit/MyFragmentAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/refoit/MyFragmentAdapter.java
@@ -26,6 +26,7 @@ public class MyFragmentAdapter extends FragmentPagerAdapter{
         this.fragments=fragments;
         this.fragmentTitles=fragmentTitles;
     }
+    
 
     @Override
     public Fragment getItem(int position) {
